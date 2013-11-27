32e8f9b89a73abd000dee240ee242a2aeb140b29
Martin <test051102@hotmail.com>
2013-11-27 14:17:30 +0100

    coding style

--- beaglebone-black/mark1_dm.c
+++ beaglebone-black/mark1_dm.c
@@ -25,6 +25,7 @@
 #include <linux/of_i2c.h>
 
 
+//SSI
 #define SSI_CLK 02 // to be verified ...
 #define SSI_DATA 04
 #define SSI_DONE 0x03
@@ -32,29 +33,28 @@
 #define SSI_INIT 0x06
 #define MODE0	0
 #define MODE1 1
+#define SSI_DELAY 300
 
+//GPIO
+#define GPIO0_BASE 0x44E07000
+#define GPIO0_SETDATAOUT *(gpio_regs+1)
+#define GPIO0_CLEARDATAOUT *(gpio_regs)
 
+//FPGA
 #define FPGA_BASE_ADDR	 0x01000000
-#define MEM_SIZE	 131072
+#define FPGA_MEM_SIZE	 131072
 
 #define DEVICE_NAME "mark1"
 
 #define I2_IO_EXP_ADDR	0x70
 
-static char * gDrvrName = DEVICE_NAME;
-static unsigned char gDrvrMajor = 0 ;
-
-unsigned char * readBuffer ;
-unsigned char * writeBuffer ;
-
 
 static int mark1_dm_open(struct inode *inode, struct file *filp);
 static int mark1_dm_release(struct inode *inode, struct file *filp);
-static ssize_t mark1_dm_write(struct file *filp, const char *buf, size_t count,
-                       loff_t *f_pos);
+static ssize_t mark1_dm_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos);
 static ssize_t mark1_dm_read(struct file *filp, char *buf, size_t count, loff_t *f_pos);
 static long mark1_dm_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
-
+int loadBitFile(struct i2c_client * io_cli, const unsigned char * bitBuffer_user, unsigned int length);
 
 
 static struct i2c_board_info io_exp_info= {
@@ -79,7 +79,6 @@ struct mark1_prog{
 	struct i2c_client * i2c_io;
 };
 
-
 struct mark1_mem{
 	unsigned short * base_addr ;
 	unsigned short * virt_addr ;
@@ -98,17 +97,17 @@ struct mark1_device{
 };
 
 
+static char * gDrvrName = DEVICE_NAME;
+static unsigned char gDrvrMajor = 0 ;
+
+unsigned char * readBuffer ;
+unsigned char * writeBuffer ;
+
 struct device * prog_device ;
 struct class * mark1_class ;
 struct mark1_device * mark1_devices ;
 
-
-int loadBitFile(struct i2c_client * io_cli, const unsigned char * bitBuffer_user, unsigned int length);
-
-
-
-
-#define SSI_DELAY 300
+volatile unsigned * gpio_regs ;
 
 
 inline void __delay_cycles(unsigned long cycles){
@@ -117,11 +116,6 @@ inline void __delay_cycles(unsigned long cycles){
 	}
 }
 
-volatile unsigned * gpio_regs ;
-
-#define GPIO0_BASE 0x44E07000
-#define GPIO0_SETDATAOUT *(gpio_regs+1)
-#define GPIO0_CLEARDATAOUT *(gpio_regs)
 inline void ssiSetClk(void){
 	//gpio_set_value(SSI_CLK, 1);
 	GPIO0_SETDATAOUT = (1 << 2) ;
@@ -145,13 +139,16 @@ inline void ssiClearData(void){
 inline void serialConfigWriteByte(unsigned char val){
 	unsigned char bitCount = 0 ;
 	unsigned char valBuf = val ;
-	for(bitCount = 0 ; bitCount < 8 ; bitCount ++){
+
+	for(bitCount = 0; bitCount < 8; bitCount ++){
 		ssiClearClk();
+
 		if((valBuf & 0x80) != 0){
 			ssiSetData();
 		}else{
 			ssiClearData();
 		}
+
 		__delay_cycles(SSI_DELAY);	
 		ssiSetClk();
 		valBuf = (valBuf << 1);
@@ -159,29 +156,29 @@ inline void serialConfigWriteByte(unsigned char val){
 	}
 }
 
-
-
-
 inline void i2c_set_pin(struct i2c_client * io_cli, unsigned char pin, unsigned char val){
-	unsigned char i2c_buffer [2] ;
+	unsigned char i2c_buffer[2];
+
 	i2c_buffer[0] = pin;
-	i2c_master_send(io_cli, i2c_buffer, 1); 
+	i2c_master_send(io_cli, i2c_buffer, 1);
 }
 
 inline unsigned char i2c_get_pin(struct i2c_client * io_cli, unsigned char pin){
-	unsigned char i2c_buffer [2] ;
+	unsigned char i2c_buffer[2];
+
 	i2c_buffer[0] = pin;
-	//i2c_master_send(io_cli, &i2c_buffer, 1); 
-	i2c_master_recv(io_cli, i2c_buffer, 2); 
+	//i2c_master_send(io_cli, &i2c_buffer, 1);
+	i2c_master_recv(io_cli, i2c_buffer, 2);
 	//printk("reading value %x \n", i2c_buffer);
-	return i2c_buffer[0] ;
+
+	return i2c_buffer[0];
 }
 
 inline unsigned char i2c_get_pin_ex(struct i2c_client * io_cli, unsigned char pin){
-	unsigned char i2c_buffer [2] ;
+	unsigned char i2c_buffer[2];
 
-	i2c_master_send(io_cli, &pin, 1); 
-	i2c_master_recv(io_cli, i2c_buffer, 2); 
+	i2c_master_send(io_cli, &pin, 1);
+	i2c_master_recv(io_cli, i2c_buffer, 2);
 
 	return i2c_buffer[1] ;
 }
@@ -192,33 +189,41 @@ int loadBitFile(struct i2c_client * io_cli, const unsigned char * bitBuffer_user
 	unsigned long int i ;
 	unsigned long int timer = 0;
 	unsigned char * bitBuffer ;	
-	unsigned char i2c_buffer [4] ;
+	//unsigned char i2c_buffer[4];
+
 	//request_mem_region(GPIO0_BASE + 0x190, 8, gDrvrName);
 	gpio_regs = ioremap_nocache(GPIO0_BASE + 0x190, 2*sizeof(int));
 
 	bitBuffer = kmalloc(length, GFP_KERNEL);
+
 	if(bitBuffer == NULL || copy_from_user(bitBuffer, bitBuffer_user, length)){
 		printk("Failed allocate buffer for configuration file \n");
-		return -ENOTTY;	
+
+		return -ENOTTY;
 	}
 
 	cfg = gpio_request(SSI_CLK, "ssi_clk");
+
 	if(cfg < 0){
 		printk("Failed to take control over ssi_clk pin \n");
+
 		return -ENOTTY;
 	}
+
 	cfg = gpio_request(SSI_DATA, "ssi_data");
+
 	if(cfg < 0){
 		printk("Failed to take control over ssi_data pin \n");
+
 		return -ENOTTY;
 	}
+
 	/*i2c_set_pin(io_cli, MODE0, 1);
 	i2c_set_pin(io_cli, MODE1, 1);
 	i2c_set_pin(io_cli, SSI_PROG, 0);*/
 
 	gpio_direction_output(SSI_CLK, 0);
 	gpio_direction_output(SSI_DATA, 0);
-
 	
 	gpio_set_value(SSI_CLK, 0);
 	//i2c_set_pin(io_cli, SSI_PROG, 1);
@@ -245,11 +250,14 @@ int loadBitFile(struct i2c_client * io_cli, const unsigned char * bitBuffer_user
 
 	timer = 0;
 	printk("Starting configuration of %d bits \n", length*8);
+
 	for(i = 0 ; i < length ; i ++){
 		serialConfigWriteByte(bitBuffer[i]);	
 		schedule();
 	}
+
 	printk("Waiting for done pin to go high \n");
+
 	while(timer < 50){
 		ssiClearClk();
 		__delay_cycles(SSI_DELAY);	
@@ -257,14 +265,17 @@ int loadBitFile(struct i2c_client * io_cli, const unsigned char * bitBuffer_user
 		__delay_cycles(SSI_DELAY);	
 		timer ++ ;
 	}
+
 	gpio_set_value(SSI_CLK, 0);
 	gpio_set_value(SSI_DATA, 1);	
+
 	if(i2c_get_pin(io_cli, SSI_DONE) == 0){
 		printk("FPGA prog failed, done pin not going high \n");
 		gpio_direction_input(SSI_CLK);
 		gpio_direction_input(SSI_DATA);
 		gpio_free(SSI_CLK);
 		gpio_free(SSI_DATA);
+
 		return -ENOTTY;		
 	}
 
@@ -278,139 +289,147 @@ int loadBitFile(struct i2c_client * io_cli, const unsigned char * bitBuffer_user
 	iounmap(gpio_regs);
 	//release_mem_region(GPIO0_BASE + 0x190, 8);
 	kfree(bitBuffer) ;
+
 	return length ;
 }
 
-
-
-ssize_t writeMem(struct file *filp, const char *buf, size_t count,
-                       loff_t *f_pos)
+ssize_t writeMem(struct file *filp, const char *buf, size_t count, loff_t *f_pos)
 {
 	unsigned short sBuf ;
 	struct mark1_mem * mem_to_write = &(((struct mark1_device *) filp->private_data)->data.mem) ;
+
 	if(count == 2){
 		if(copy_from_user(&sBuf, buf, count)) return -1 ;
-		mem_to_write->virt_addr[(*f_pos)/2] = sBuf ; 
+		mem_to_write->virt_addr[(*f_pos)/2] = sBuf ;
 		return count ;	
 	}
+
 	if (copy_from_user((void *) &(mem_to_write->virt_addr[(*f_pos)/2]), buf, count) ) {
 		return -1 ;		
 	}
+
 	return count;
 }
-
 
 ssize_t readMem(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
 	struct mark1_mem * mem_to_read = &(((struct mark1_device *) filp->private_data)->data.mem) ;
+
 	if (copy_to_user(buf, (void *) &(mem_to_read->virt_addr[(*f_pos)/2]), count) ) {
 		return -1 ;
 	}
+
 	return count;
 }
 
-
-
 static int mark1_dm_open(struct inode *inode, struct file *filp)
 {
-	struct mark1_device * dev ;
+	struct mark1_device * dev = container_of(inode->i_cdev, struct mark1_device, cdev);
 	struct mark1_mem * mem_dev ;
-	dev = container_of(inode->i_cdev, struct mark1_device, cdev);
+
 	filp->private_data = dev; /* for other methods */
+
 	if(dev == NULL){
 		printk("Failed to retrieve mark1 structure !\n");
+
 		return -1 ;	
 	}
+
 	if(dev->opened == 1){
 		printk("%s: module already opened\n", gDrvrName);
+
 		return 0 ;
 	}
 
-	if(dev->type == prog){
-		
-	}else{
+	if(dev->type != prog){
 		mem_dev = &((dev->data).mem) ;
-		request_mem_region((unsigned long) mem_dev->base_addr, MEM_SIZE, gDrvrName);
-		mem_dev->virt_addr = ioremap_nocache(((unsigned long) mem_dev->base_addr), MEM_SIZE);
+		request_mem_region((unsigned long) mem_dev->base_addr, FPGA_MEM_SIZE, gDrvrName);
+		mem_dev->virt_addr = ioremap_nocache(((unsigned long) mem_dev->base_addr), FPGA_MEM_SIZE);
 		printk("mem interface opened \n");	
 	}
-	dev->opened = 1 ;
+
+	dev->opened = 1;
+
 	return 0;
 }
 
 static int mark1_dm_release(struct inode *inode, struct file *filp)
 {
-	struct mark1_device * dev ;
-	dev = container_of(inode->i_cdev, struct mark1_device, cdev);
+	struct mark1_device * dev = container_of(inode->i_cdev, struct mark1_device, cdev);;
+
 	if(dev->opened == 0){
 		printk("%s: module already released\n", gDrvrName);
 		return 0 ;
 	}
+
 	if(dev->type == mem){
 		iounmap((dev->data.mem).virt_addr);
-		release_mem_region(((unsigned long) (dev->data.mem).base_addr), MEM_SIZE );
+		release_mem_region(((unsigned long) (dev->data.mem).base_addr), FPGA_MEM_SIZE );
 		printk("%s: Release: module released\n",gDrvrName);
 	}
+
 	dev->opened = 0 ;
+
 	return 0;
 }
 
-
-static ssize_t mark1_dm_write(struct file *filp, const char *buf, size_t count,
-                       loff_t *f_pos)
+static ssize_t mark1_dm_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos)
 {
-	struct mark1_device * dev ;
-	dev = filp->private_data ; /* for other methods */
+	struct mark1_device * dev = filp->private_data ; /* for other methods */
+
 	switch(dev->type){
-		case prog :
-			
+		case prog :		
 			return loadBitFile((dev->data.prog.i2c_io), buf, count);
+
 		case mem:
 			return writeMem(filp, buf, count, f_pos);
+
 		default:
 			return loadBitFile((dev->data.prog.i2c_io), buf, count);	
-	};
-	
+	};	
 }
-
 
 static ssize_t mark1_dm_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
-	struct mark1_device * dev ;
-	dev = filp->private_data ; /* for other methods */
+	struct mark1_device * dev = filp->private_data ; /* for other methods */
+
 	switch(dev->type){
 		case prog :
 			return -1;
+
 		case mem:
 			return readMem(filp, buf, count, f_pos);
+
 		default:
 			return -1 ;	
 	};
 }
 
-
 static long mark1_dm_ioctl(struct file *filp, unsigned int cmd, unsigned long arg){
 	printk("ioctl failed \n");
+
 	return -ENOTTY;
 }
-
-
 
 static void mark1_dm_exit(void)
 {
 	int i;
 	dev_t devno = MKDEV(gDrvrMajor, 0);
+
 	/* Get rid of our char dev entries */
 	if (mark1_devices) {
 		for (i = 0; i < 2; i++) {
 			if(i == 0){
 				i2c_unregister_device(mark1_devices[i].data.prog.i2c_io);			
 			}
+
 			device_destroy(mark1_class, MKDEV(gDrvrMajor, i));
 			cdev_del(&mark1_devices[i].cdev);
 		}
+
 		kfree(mark1_devices);
 	}
+
 	class_destroy(mark1_class);
 	/* cleanup_module is never called if registering failed */
 	unregister_chrdev_region(devno, 2);
@@ -418,28 +437,29 @@ static void mark1_dm_exit(void)
 
 static int mark1_dm_init(void)
 {
-		
 	int result;
 	int devno ;
 	struct mark1_mem * memDev ;
 	struct mark1_prog * progDev ;
 	struct i2c_adapter *i2c_adap;
+
         dev_t dev = 0;
-	result = alloc_chrdev_region(&dev, 0, 2,
-                                gDrvrName);
+	result = alloc_chrdev_region(&dev, 0, 2, gDrvrName);
         gDrvrMajor = MAJOR(dev);
+
         if (result < 0) {
                  printk(KERN_ALERT "Registering char device failed with %d\n", gDrvrMajor);
                 return result;
         }
+
         mark1_devices = kmalloc(2 * sizeof(struct mark1_device), GFP_KERNEL);
+
         if (! mark1_devices) {
                 result = -ENOMEM;
                 goto fail;  /* Make this more graceful */
         }
 
 	mark1_class = class_create(THIS_MODULE,DEVICE_NAME);
-
         memset(mark1_devices, 0, 2 * sizeof(struct mark1_device));
 	
 	/*Initializing main mdevice for prog*/
@@ -449,14 +469,14 @@ static int mark1_dm_init(void)
 	prog_device = device_create(mark1_class, NULL, devno, NULL, DEVICE_NAME);	// should create /dev entry for main node
 	mark1_devices[0].opened = 0 ;
 	
-	/*
-	Do the i2c stuff
-	*/
+	/*Do the i2c stuff*/
 	i2c_adap = i2c_get_adapter(1); // todo need to check i2c adapter id
+
 	if(i2c_adap == NULL){
 		printk("Cannot get adapter 1 \n");
 		goto fail ;
 	}
+
 	progDev->i2c_io = i2c_new_device(i2c_adap , &io_exp_info);
 	i2c_put_adapter(i2c_adap); //don't know what it does, seems to release the adapter ...
 	
@@ -466,6 +486,7 @@ static int mark1_dm_init(void)
 		mark1_devices[0].opened = 0 ;
                 goto fail;
 	}
+
 	cdev_init(&(mark1_devices[0].cdev), &mark1_dm_ops);
 	mark1_devices[0].cdev.owner = THIS_MODULE;
 	mark1_devices[0].cdev.ops = &mark1_dm_ops;
@@ -476,15 +497,18 @@ static int mark1_dm_init(void)
 	mark1_devices[1].type = mem ;
 	memDev = &(mark1_devices[1].data.mem);
 	memDev->base_addr = (unsigned short *) (FPGA_BASE_ADDR);
-	device_create(mark1_class, prog_device, devno, NULL, "mark1_mem"); 
+	device_create(mark1_class, prog_device, devno, NULL, "mark1_mem");
 	cdev_init(&(mark1_devices[1].cdev), &mark1_dm_ops);
 	(mark1_devices[1].cdev).owner = THIS_MODULE;
 	(mark1_devices[1].cdev).ops = &mark1_dm_ops;
 	cdev_add(&(mark1_devices[1].cdev), devno, 1);
 	mark1_devices[1].opened = 0 ;
+
 	return 0 ;
-  fail:
+
+fail:
         mark1_dm_exit();
+
         return -1 ;
 
 }
@@ -493,9 +517,8 @@ static const struct of_device_id mark1_of_match[] = {
 	{ .compatible = "mark1", },
 	{ },
 };
+
 MODULE_DEVICE_TABLE(of, mark1_of_match);
-
-
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Jonathan Piat <piat.jonathan@gmail.com>");
 
