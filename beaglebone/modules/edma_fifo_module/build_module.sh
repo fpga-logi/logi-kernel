1cec8dafc59ef1188dfb69aed5448e018e02954e
Jonathan Piat <piat.jonathan@gmail.com>
2014-02-24 12:46:55 +0100

    cleaning respository

--- beaglebone/modules/edma_fifo_module/build_module.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- KERNELDIR=~/development/KERNEL/ARM/beaglebone/kernels/ti33x-psp-3.1/
--- beaglebone/modules/fifo_module/build_module.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-CC_PATH=/home/jpiat/development/KERNEL/ARM/beaglebone/toolchain/arm-2011.03/bin/
-make ARCH=arm CROSS_COMPILE=${CC_PATH}arm-none-linux-gnueabi- KERNELDIR=~/development/KERNEL/ARM/beaglebone/kernels/ti33x-psp-3.1/
