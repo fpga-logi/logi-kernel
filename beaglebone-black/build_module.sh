12aa1ca0f312eef1d9a7df7e17e10b45df1e8927
Jonathan Piat <piat.jonathan@gmail.com>
2014-04-09 18:35:40 +0200

    trying to ease module compilation

--- beaglebone-black/build_module.sh
+++ beaglebone-black/build_module.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 
+LINUX_DEV=$1
 
-CC_PREFIX=$1 #fill with the location of toolchain and target triplet
-KERNEL_DIR=$2 #fill with the location of you kernel sources
+CC_PREFIX=${LINUX_DEV}/dl/gcc-linaro-arm-linux-gnueabihf-4.7-2013.04-20130415_linux/bin/arm-linux-gnueabihf-
+KERNEL_DIR=${LINUX_DEV}/KERNEL
 
 
-cd logibone_ra1
+cd logibone_r1
 make clean
 make ARCH=arm CROSS_COMPILE=${CC_PREFIX} KERNELDIR=${KERNEL_DIR}
-cd ../logibone_ra2
-make clean
-make ARCH=arm CROSS_COMPILE=${CC_PREFIX} KERNELDIR=${KERNEL_DIR}
-cd ../mark1
-make clean
-make ARCH=arm CROSS_COMPILE=${CC_PREFIX} KERNELDIR=${KERNEL_DIR}
-
+cd ..
+mkdir deploy
+cp  logibone_r1/logibone_r1_dma.ko deploy/
+cp logibone_r1/setup_device-tree_R1.sh deploy/
+cp logibone_r1/BB-BONE-LOGIBONE-00R1.dts deploy/
