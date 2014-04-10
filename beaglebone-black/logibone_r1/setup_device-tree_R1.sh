240508f2822b9614c91ea6dd9b73b1b8be910756
Jonathan Piat <piat.jonathan@gmail.com>
2014-04-10 08:42:56 +0200

    small change to auto-detect cape manager directory

--- beaglebone-black/logibone_r1/setup_device-tree_R1.sh
+++ beaglebone-black/logibone_r1/setup_device-tree_R1.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+
+
+CAPE_MNGR="$(ls /sys/devices/ | grep bone_capemgr)"
+
 rm /lib/firmware/BB-BONE-LOGIBONE-00R1.dtbo
 dtc -O dtb -o BB-BONE-LOGIBONE-00R1.dtbo -b 0 -@ BB-BONE-LOGIBONE-00R1.dts
 cp BB-BONE-LOGIBONE-00R1.dtbo /lib/firmware
-sh -c "echo -4 > /sys/devices/bone_capemgr.9/slots "
-sh -c "echo -5 > /sys/devices/bone_capemgr.9/slots "
-sh -c "echo -6 > /sys/devices/bone_capemgr.9/slots "
-sh -c "echo BB-BONE-LOGIBONE:00R1 > /sys/devices/bone_capemgr.9/slots "
-cat  /sys/devices/bone_capemgr.9/slots
+sh -c "echo -4 > /sys/devices/"${CAPE_MNGR}"/slots "
+sh -c "echo -5 > /sys/devices/"${CAPE_MNGR}"/slots "
+sh -c "echo -6 > /sys/devices/"${CAPE_MNGR}"/slots "
+sh -c "echo BB-BONE-LOGIBONE:00R1 > /sys/devices/"${CAPE_MNGR}"/slots "
+cat  /sys/devices/${CAPE_MNGR}/slots
 insmod logibone_r1_dma.ko
