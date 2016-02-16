e2fcdf3ca001cc63c89b937845336f7798d7ee83
QuickJack <test051102@hotmail.com>
2016-02-16 10:49:44 +0100

    Device tree fixes for Kernel 4.1.x

--- beaglebone-black/logibone_r1/setup_device-tree_R1.sh
+++ beaglebone-black/logibone_r1/setup_device-tree_R1.sh
@@ -2,12 +2,21 @@
 
 CAPE_MNGR="$(ls /sys/devices/ | grep bone_capemgr)"
 
+if [ -z "$CAPE_MNGR" ]; then
+	SLOTS=/sys/devices/platform/bone_capemgr/slots
+else
+	SLOTS=/sys/devices/$CAPE_MNGR/slots
+fi
+
 rm /lib/firmware/BB-BONE-LOGIBONE-00R1.dtbo
 dtc -O dtb -o BB-BONE-LOGIBONE-00R1.dtbo -b 0 -@ BB-BONE-LOGIBONE-00R1.dts
 cp BB-BONE-LOGIBONE-00R1.dtbo /lib/firmware
-#sh -c "echo -4 > /sys/devices/"${CAPE_MNGR}"/slots "
-#sh -c "echo -5 > /sys/devices/"${CAPE_MNGR}"/slots "
-#sh -c "echo -6 > /sys/devices/"${CAPE_MNGR}"/slots "
-sh -c "echo BB-BONE-LOGIBONE:00R1 > /sys/devices/"${CAPE_MNGR}"/slots "
-cat  /sys/devices/${CAPE_MNGR}/slots
+
+#sh -c "echo -4 > ${SLOTS}"
+#sh -c "echo -5 > ${SLOTS}"
+#sh -c "echo -6 > ${SLOTS}"
+sh -c "echo BB-BONE-LOGIBONE:00A3 > ${SLOTS}"
+
+cat ${SLOTS} 
+
 insmod logibone_r1_dma.ko
